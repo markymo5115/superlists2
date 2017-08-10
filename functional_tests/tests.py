af78b899910619e01b0129a745345ff82c47053d
mark <mark@mdmorris.co.uk>
2017-08-10 09:26:24 +0100

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -68,7 +68,6 @@ class NewVisitorTest(LiveServerTestCase):
 # She visits that URL - her to-do list is still there.
 
 # Satisfied, she goes back to sleep
-		self.fail('Finish the test!')
 
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 		#Edit starts a new to-do list
@@ -113,6 +112,30 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('Buy milk', page_text)
 
 		# Satisfied, they both go back to sleep
+
+	def test_layout_and_styling(self):
+		#Edith goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024,768)
+
+		#She notices the input box is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2, 
+			512, 
+			delta = 50
+			)
+		# She starts a new list and sees the input is nicely centered			# there too
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta = 50
+		)
+
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
 
