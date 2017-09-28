c5016ceefd0659cfcb72bba2bde2829ec5a62750
mark <mark@mdmorris.co.uk>
2017-09-28 06:09:01 +0100

    FT ready to do some javascript

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,6 +4,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+	def get_error_element(self):
+		return self.browser.find_element_by_css_selector('.has-error')
+
 	def test_cannot_add_empty_list_items(self):
 		# Edith goes to the home page and accidentally tries to submit
 		# an empty list item. She hits Enter on the empty input box
@@ -66,24 +69,28 @@ class ItemValidationTest(FunctionalTest):
 
 		#Whe sees a helpful error message
 		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_elements_by_css_selector(
-				'.has-error'),
+			self.get_error_element().text,
 				"You've already got this in your list"
 		))
-		
-		#Edith enters a different item
-		#self.get_item_input_box().send_keys('Buy chocolate')
-		#The site accepts this
-		#self.wait_for(lambda:
-		#	self.browser.find_elements_by_css_selector(
-		#		'#id_text:valid'
-		#	))
+	
+	def test_error_messages_are_cleared_on_input(self):
+		# Edith starts a list and casuses a validation error:
+		self.browser.get(self.live_server_url)
+		self.get_item_input_box().send_keys('Buy choc')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy choc')
+		self.get_item_input_box().send_keys('Buy choc')
+		self.get_item_input_box().send_keys(Keys.ENTER)		
 
-		#Edith adds the first item again to see if the site still 
-		#recognises a duplication
-		#self.get_item_input_box().send_keys('Buy fudge')
-		#The site tells her that she has already entered this item
-		#self.wait_for(lambda:
-		#	self.browser.find_elements_by_css_selector(
-		#		'#id_text:valid'
-		#	))		
+		self.wait_for(lambda: self.assertTrue(
+			self.get_error_element().is_displayed()
+				))
+
+		# She starts typing in the input box to clear the error
+		self.get_item_input_box().send_keys('a')
+
+		# She is pleased to see that the error message disappears	
+		self.wait_for(lambda: self.assertFalse(
+			self.get_error_element().is_displayed()
+			))
+		
